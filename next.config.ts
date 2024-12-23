6daf7169d27871c20216ce47b24b0c03395d5352
Abhishek Singh Kushwah <--add>
2024-12-24 01:25:27 +0530

    Changes to add

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,28 @@
-const isProd = process.env.NODE_ENV === 'production';
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/rand0wn.github.io",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true,
   },
-  assetPrefix: isProd ? '/rand0wn.github.io/' : '',
-  basePath: isProd ? '/rand0wn.github.io' : '',
-  output: 'export'
 };
 
 export default nextConfig;
